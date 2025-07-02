c0e6c4a2bdf676a60545a7192d543f3fc4c0cf2c
Rantolj <rantolanja@gmail.com>
2025-07-02 18:47:18 +0300

    ok

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,9 +3,6 @@
 //importation de controller
 use app\controllers\GroupeControllers\GroupeController;
 use app\controllers\GroupeControllers\ReservationController;
-use app\controllers\GroupeControllers\AbonnementController;
-use app\controllers\GroupeControllers\TarifAbonnementController;
-use app\controllers\GroupeControllers\TarifClubController;
 
 //importation lié flight
 use flight\Engine;
@@ -24,9 +21,7 @@ use flight\net\Router;
 
 $GroupeController = new GroupeController();
 $ReservationController = new ReservationController();
-$AbonnementController = new AbonnementController();
-$TarifAbonnementController = new TarifAbonnementController();
-$TarifClubController = new TarifClubController();
+
 
 $router->get('/test', function() {
     Flight::json(['message' => 'Test OK']);
@@ -48,31 +43,7 @@ $router->post('/reservation/update/@id:[0-9]+', [ $ReservationController, 'Updat
 $router->get('/reservation/delete/@id:[0-9]+', [ $ReservationController, 'DeleteReservation' ]);
 
 
-$router->get('/abonnements', [ $AbonnementController, 'index' ]);
-$router->get('/abonnement/@id:[0-9]+', [ $AbonnementController, 'show' ]);
-$router->get('/abonnement/insert', [ $AbonnementController, 'createForm' ]);
-$router->post('/abonnement/insert', [ $AbonnementController, 'create' ]);
-$router->post('/abonnement/update/@id:[0-9]+', [ $AbonnementController, 'update' ]);
-$router->get('/abonnement/delete/@id:[0-9]+', [ $AbonnementController, 'delete' ]);
-$router->get('/abonnement/renouveler/@id:[0-9]+', [ $AbonnementController, 'renouveler' ]);
-$router->get('/abonnement/annuler/@id:[0-9]+', [ $AbonnementController, 'annuler' ]);
-$router->get('/abonnements/rappel', [ $AbonnementController, 'rappelAutomatique' ]);
-$router->get('/abonnement/facture/@id:[0-9]+', [ $AbonnementController, 'facture' ]);
-$router->get('/abonnements/historique', [ $AbonnementController, 'historique' ]);
 
-
-$router->get('/tarifs', [ $TarifAbonnementController, 'index' ]);
-$router->get('/tarif/@id:[0-9]+', [ $TarifAbonnementController, 'show' ]);
-$router->post('/tarif/insert', [ $TarifAbonnementController, 'create' ]);
-$router->post('/tarif/update/@id:[0-9]+', [ $TarifAbonnementController, 'update' ]);
-$router->get('/tarif/delete/@id:[0-9]+', [ $TarifAbonnementController, 'delete' ]);
-
-
-$router->get('/tarifs-club', [ $TarifClubController, 'index' ]);
-$router->get('/tarif-club/@id:[0-9]+', [ $TarifClubController, 'show' ]);
-$router->post('/tarif-club/insert', [ $TarifClubController, 'create' ]);
-$router->post('/tarif-club/update/@id:[0-9]+', [ $TarifClubController, 'update' ]);
-$router->get('/tarif-club/delete/@id:[0-9]+', [ $TarifClubController, 'delete' ]);
 // $router->get('/', \app\controllers\WelcomeController::class.'->home'); 
 
 // $router->get('/hello-world/@name', function($name) {
